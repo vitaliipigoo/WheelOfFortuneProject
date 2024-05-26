3d9dac9f106515b4403749154a223ba33f644385
Vitalii Mamchur <146951542+vitaliipigoo@users.noreply.github.com>
2024-05-26 15:42:40 +0300

    Scene updates

--- WoL example/Assets/Scripts/Installers/AppInstaller.cs
+++ WoL example/Assets/Scripts/Installers/AppInstaller.cs
@@ -1,13 +1,35 @@
+using Common;
 using Services;
+using UnityEngine;
 using Zenject;
 
 namespace Installers
 {
     public class AppInstaller : MonoInstaller
     {
+        [SerializeField] private Transform canvas;
+        [SerializeField] private LaunchButton launchButton;
+        
         public override void InstallBindings()
         {
+            BindServices();
+            BindMonoBehaviours();
+        }
+
+        private void BindServices()
+        {
             Container.Bind<IInjectedPrefabsService>().To<InjectedPrefabsService>().AsSingle();
+        }
+
+        private void BindMonoBehaviours()
+        {
+            InitGameObjectWithComponent<LaunchButton>(launchButton);
+        }
+        
+        private void InitGameObjectWithComponent<T>(Object prefab) where T : Component
+        {
+            var target = Container.InstantiatePrefabForComponent<T>(prefab, canvas.transform);
+            DontDestroyOnLoad(target.gameObject);
         }
     }
 }
